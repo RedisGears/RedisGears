1b71218f0e4fbfe51a17386d69de62722e48804f
rafie <rafi@redislabs.com>
2019-09-25 16:48:30 +0300

    Fixed crash in Cluster_ConnectToShard() + readies updates

--- deps/readies/paella/debug.py
+++ deps/readies/paella/debug.py
@@ -3,8 +3,11 @@ import os
 
 #----------------------------------------------------------------------------------------------
 
-if 'PYDEBUG' in os.environ:
-	from pdb import set_trace as bb
+if 'PYDEBUG' in os.environ and os.environ['PYDEBUG'] == '1':
+    try:
+        from pudb import set_trace as bb
+    except ImportError:
+        from pdb import set_trace as bb
 else:
 	def bb(): pass
 
