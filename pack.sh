4fdaf7193c324ba16e83469890c93e681513b9a8
rafie <rafi@redislabs.com>
2020-04-02 00:51:44 +0300

    Build system 2

--- pack.sh
+++ pack.sh
@@ -70,12 +70,13 @@ pack() {
 	local artifact="$1"
 	local vertag="$2"
 
-	cd $ROOT
+	cd $CPYTHON_PREFIX
 
 	local ramp="$(command -v python) -m RAMP.ramp" 
 	local packfile=$PACKAGE_NAME.$OS-$OSNICK-$ARCH.$vertag.zip
 	local packname=$(GEARS_NO_DEPS=1 $ramp pack -m ramp.yml -o $packfile $GEARS_SO | tail -1)
 
+	cd $ROOT
 	if [[ -z $packname ]]; then
 		echo Failed to pack $artifact
 		exit 1
