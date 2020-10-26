e4e5e51088e0b4157e92d1b8f424e5585ecd3113
meir@redislabs.com <meir@redislabs.com>
2020-10-26 15:08:35 +0200

    Fix failing tests

---
+++ src/mappers.c
@@ -0,0 +1,20 @@
+#include "redismodule.h"
+#include "utils/arr_rm_alloc.h"
+#include <assert.h>
+#include <string.h>
+#include "redisgears.h"
+#include "redisgears_memory.h"
+#include "record.h"
+
+
+Record* GetValueMapper(ExecutionCtx* rctx, Record *record, void* arg){
+    if(RedisGears_RecordGetType(record) != keyRecordType){
+        RedisGears_SetError(rctx, RG_STRDUP("can not extract value for a none key value record."));
+        RedisGears_FreeRecord(record);
+        return NULL;
+    }
+    Record* res = RedisGears_KeyRecordGetVal(record);
+    RedisGears_KeyRecordSetVal(record, NULL);
+    RedisGears_FreeRecord(record);
+    return res;
+}
---
+++ src/mappers.h
@@ -0,0 +1,16 @@
+/*
+ * mappers.h
+ *
+ *  Created on: May 6, 2019
+ *      Author: root
+ */
+
+#ifndef SRC_MAPPERS_H_
+#define SRC_MAPPERS_H_
+
+#include "redisgears.h"
+
+Record* GetValueMapper(ExecutionCtx* rctx, Record *record, void* arg);
+
+
+#endif /* SRC_MAPPERS_H_ */
