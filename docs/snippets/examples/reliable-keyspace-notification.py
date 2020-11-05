9111fb85c1d32c7036ef05c6556306ff1e4be003
Meir Shpilraien (Spielrein) <meir@redislabs.com>
2020-11-05 09:03:16 +0200

    Update reliable-keyspace-notification.py

--- docs/snippets/examples/reliable-keyspace-notification.py
+++ docs/snippets/examples/reliable-keyspace-notification.py
@@ -1,3 +1,3 @@
 GearsBuilder() \
 .foreach(lambda x: execute('XADD', "notifications-stream", '*', *sum([[k,v] for k,v in x.items()],[]))) \
-.register(prefix="person:*", eventTypes=['hset', 'hmset'])
+.register(prefix="person:*", eventTypes=['hset', 'hmset'], mode='sync')
