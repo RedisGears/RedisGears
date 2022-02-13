bdb616960845b67fcfce9c6894f16422928d13a4
meir <meir@redis.com>
2022-02-13 16:03:30 +0200

    fix build

--- plugins/jvmplugin/system-setup.py
+++ plugins/jvmplugin/system-setup.py
@@ -23,9 +23,6 @@ class JVMSetup(paella.Setup):
     def common_last(self):
         self.install("maven")
         self.run("{PYTHON} -m pip install --upgrade pip".format(PYTHON=self.python, READIES=READIES))
-        self.run("{PYTHON} {READIES}/bin/getrmpytools".format(PYTHON=self.python, READIES=READIES))
-        self.pip_uninstall("redis redis-py-cluster ramp-packer RLTest")
-        self.pip_install("git+https://github.com/RedisLabsModules/RLTest.git@v0.4.2")
 
 #----------------------------------------------------------------------------------------------
 
