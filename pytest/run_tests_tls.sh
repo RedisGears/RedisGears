5ac8b3691d3387a3e9e46e0b53b157ed27c17d9d
meir <meir@redis.com>
2022-04-05 13:18:27 +0300

    review fixes

--- pytest/run_tests_tls.sh
+++ pytest/run_tests_tls.sh
@@ -2,6 +2,6 @@
 
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-$HERE/generate_tests_cert.sh
+${HERE}/generate_tests_cert.sh
 
-TLS=1 $HERE/run_tests.sh "$@"
+TLS=1 ${HERE}/run_tests.sh "$@"
