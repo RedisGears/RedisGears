d1120dad965558e2549b72338284c6457c23a1df
meir <meir@redis.com>
2023-01-10 14:55:26 +0200

    Review fixes

--- append_deps.py
+++ append_deps.py
@@ -2,6 +2,7 @@ import zipfile
 import os
 import shutil
 import json
+import tempfile
 
 directories = ["artifacts/release/", "artifacts/snapshot/"]
 
@@ -10,21 +11,19 @@ BASE_DIR = dir_path = os.path.dirname(os.path.realpath(__file__))
 for dir in directories:
     dir = os.path.join(BASE_DIR, dir)
     os.chdir(dir)
-    for f in os.listdir('./'):
+    for f in [a for a in os.listdir('./')]:
         name, ext = os.path.splitext(f)
         if ext == '.zip' and 'withdeps' not in name:
-            print('Appending deps for %s' % name)
-            os.mkdir('./tmp/')
-            os.chdir('./tmp/')
-            os.mkdir('./deps/')
-            with zipfile.ZipFile(os.path.join('../', f), 'r') as zip_ref:
-                zip_ref.extractall("./")
-                f = open('./module.json')
-                data = json.load(f)
-                for dep in data['dependencies'].values():
-                    url = dep['url']
-                    deps_file_name = url.split('/')[-1]
-                    shutil.copy(os.path.join('../', deps_file_name), os.path.join('./deps/', deps_file_name))
-            shutil.make_archive(os.path.join('../', '%s-withdeps' % name), 'zip', './')
-            os.chdir('../')
-            shutil.rmtree('./tmp/')
+            with tempfile.TemporaryDirectory() as tmpdirname:
+                print(f"Appending deps for {name}, working dir {tmpdirname}")
+                os.chdir(tmpdirname)
+                os.makedirs('./deps/')
+                with zipfile.ZipFile(os.path.join(dir, f), 'r') as zip_ref:
+                    zip_ref.extractall("./")
+                    data = json.load(open('./module.json'))
+                    for dep in data['dependencies'].values():
+                        url = dep['url']
+                        deps_file_name = url.split('/')[-1]
+                        shutil.copy(os.path.join(dir, deps_file_name), os.path.join(os.getcwd(), 'deps', deps_file_name))
+                shutil.make_archive(os.path.join(dir, '%s-withdeps' % name), 'zip', './')
+                os.chdir(dir)
