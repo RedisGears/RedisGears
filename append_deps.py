798f6aaf16980f480b5c05806ec3e92bc29c4cd8
meir <meir@redis.com>
2023-01-10 17:58:50 +0200

    review fixes

--- append_deps.py
+++ append_deps.py
@@ -10,14 +10,13 @@ BASE_DIR = dir_path = os.path.dirname(os.path.realpath(__file__))
 
 for dir in directories:
     dir = os.path.join(BASE_DIR, dir)
-    os.chdir(dir)
-    for f in [a for a in os.listdir('./')]:
+    for f in [a for a in os.listdir(dir)]:
         name, ext = os.path.splitext(f)
         if ext == '.zip' and 'withdeps' not in name:
             with tempfile.TemporaryDirectory() as tmpdirname:
                 print(f"Appending deps for {name}, working dir {tmpdirname}")
-                os.chdir(tmpdirname)
-                os.makedirs('./deps/')
+                os.chdir(os.path.abspath(tmpdirname))
+                os.makedirs('deps')
                 with zipfile.ZipFile(os.path.join(dir, f), 'r') as zip_ref:
                     zip_ref.extractall("./")
                     data = json.load(open('./module.json'))
@@ -26,4 +25,3 @@ for dir in directories:
                         deps_file_name = url.split('/')[-1]
                         shutil.copy(os.path.join(dir, deps_file_name), os.path.join(os.getcwd(), 'deps', deps_file_name))
                 shutil.make_archive(os.path.join(dir, '%s-withdeps' % name), 'zip', './')
-                os.chdir(dir)
