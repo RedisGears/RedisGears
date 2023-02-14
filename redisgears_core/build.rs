87d4ab874bb66fca6f4cc9aa80aa47d2b08a4866
meir <meir@redis.com>
2023-02-14 10:13:15 +0200

    extract major os version only on rhel like builds.

--- redisgears_core/build.rs
+++ redisgears_core/build.rs
@@ -47,20 +47,14 @@ fn main() {
     let rhel_like_os = vec!["centos".to_string(), "rocky".to_string()];
 
     let os_type = info.os_type().to_string().to_lowercase();
-    let os_type = if rhel_like_os.contains(&os_type) {
-        "rhel".to_string()
+    let (os_type, os_version) = if rhel_like_os.contains(&os_type) {
+        ("rhel".to_string(), info.version().to_string().split(".").into_iter().next().expect("Failed getting os version").to_string())
     } else {
-        os_type
+        (os_type, info.version().to_string())
     };
     println!("cargo:rustc-env=BUILD_OS_TYPE={}", os_type);
     println!(
-        "cargo:rustc-env=BUILD_OS_VERSION={}",
-        info.version()
-            .to_string()
-            .split(".")
-            .into_iter()
-            .next()
-            .expect("Failed getting os version")
+        "cargo:rustc-env=BUILD_OS_VERSION={}", os_version
     );
     println!("cargo:rustc-env=BUILD_OS_ARCH={}", std::env::consts::ARCH);
     println!(
