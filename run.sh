62a7de8bd28e734604d3eb25ccccc68a32cab05e
Victor Polevoy <fx@thefx.co>
2023-02-28 18:21:48 +0100

    Add some comments and more shorthands in run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,14 @@
+# Run under release
 #redis-server --loadmodule ./target/release/libredisgears.so ./target/release/libredisgears_v8_plugin.so
+
+# Run under debug
 redis-server --loadmodule ./target/debug/libredisgears.so ./target/debug/libredisgears_v8_plugin.so --enable-debug-command yes
+
+# Run some locally-built redis server with the debug library.
+#../redis/src/redis-server --loadmodule ./target/debug/libredisgears.so ./target/debug/libredisgears_v8_plugin.so --enable-debug-command yes
+
+# Run the redis-server built locally with the module under GDB for immediate debugging.
+#gdb --args ../redis/src/redis-server --loadmodule ./target/debug/libredisgears.so ./target/debug/libredisgears_v8_plugin.so --enable-debug-command yes
+
+# Run valgrind with the debug module and redis-server in $PATH.
 #valgrind --leak-check=full redis-server --loadmodule ./target/debug/libredisgears.so ./target/debug/libredisgears_v8_plugin.so
