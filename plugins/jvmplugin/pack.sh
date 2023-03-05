00b214a305710a1e4ad36c66f847603d524b0fd1
rafie <rafi@redislabs.com>
2023-03-05 22:42:58 +0200

    fixes 7

--- plugins/jvmplugin/pack.sh
+++ plugins/jvmplugin/pack.sh
@@ -3,6 +3,9 @@
 set -e
 
 # consistency - lifted from pack.sh, until we rewrite
+[[ $ARCH == x64 ]] && ARCH=x86_64
+[[ $OS == linux ]] && OS=Linux
+
 [[ $OSNICK == trusty ]]  && OSNICK=ubuntu14.04
 [[ $OSNICK == xenial ]]  && OSNICK=ubuntu16.04
 [[ $OSNICK == bionic ]]  && OSNICK=ubuntu18.04
@@ -10,8 +13,8 @@ set -e
 [[ $OSNICK == jammy ]]   && OSNICK=ubuntu22.04
 [[ $OSNICK == centos7 ]] && OSNICK=rhel7
 [[ $OSNICK == centos8 ]] && OSNICK=rhel8
-[[ $OSNICK == ol8 ]] && OSNICK=rhel8
-[[ $OSNICK == rocky8 ]] && OSNICK=rhel8
+[[ $OSNICK == ol8 ]]     && OSNICK=rhel8
+[[ $OSNICK == rocky8 ]]  && OSNICK=rhel8
 
 [[ $OSNICK == bigsur ]]  && OSNICK=catalina
 
@@ -21,16 +24,18 @@ else
     JVM_PATH=./bin/OpenJDK/jdk-11.0.9.1+1/
 fi
 
-tar -czf ../../artifacts/snapshot/redisgears-jvm.Linux-$OSNICK-x86_64.$GIT_BRANCH.tgz \
---transform "s,^./src/,./plugin/," \
-$JVM_PATH/* \
-./src/gears_jvm.so \
-./gears_runtime/target/gear_runtime-jar-with-dependencies.jar
-sha256sum ../../artifacts/snapshot/redisgears-jvm.Linux-$OSNICK-x86_64.$GIT_BRANCH.tgz |cut -d ' ' -f 1-1 > ../../artifacts/snapshot/redisgears-jvm.Linux-$OSNICK-x86_64.$GIT_BRANCH.tgz.sha256
+BRANCH_PLUGIN_TAR=../../artifacts/snapshot/redisgears-jvm.${OS}-${OSNICK}-${ARCH}.$GIT_BRANCH.tgz
+tar -czf $BRANCH_PLUGIN_TAR \
+	--transform "s,^./src/,./plugin/," \
+	$JVM_PATH/* \
+	./src/gears_jvm.so \
+	./gears_runtime/target/gear_runtime-jar-with-dependencies.jar
+sha256sum $BRANCH_PLUGIN_TAR | cut -d ' ' -f 1-1 > $BRANCH_PLUGIN_TAR.sha256
 
-tar -czf ../../artifacts/release/redisgears-jvm.Linux-$OSNICK-x86_64.$VERSION.tgz \
---transform "s,^./src/,./plugin/," \
-$JVM_PATH/* \
-./src/gears_jvm.so \
-./gears_runtime/target/gear_runtime-jar-with-dependencies.jar
-sha256sum ../../artifacts/release/redisgears-jvm.Linux-$OSNICK-x86_64.$VERSION.tgz | cut -d ' ' -f 1-1 > ../../artifacts/release/redisgears-jvm.Linux-$OSNICK-x86_64.$VERSION.tgz.sha256
+VERSION_PLUGIN_TAR=../../artifacts/release/redisgears-jvm.${OS}-${OSNICK}-${ARCH}.$VERSION.tgz
+tar -czf $VERSION_PLUGIN_TAR \
+	--transform "s,^./src/,./plugin/," \
+	$JVM_PATH/* \
+	./src/gears_jvm.so \
+	./gears_runtime/target/gear_runtime-jar-with-dependencies.jar
+sha256sum $VERSION_PLUGIN_TAR | cut -d ' ' -f 1-1 > $VERSION_PLUGIN_TAR.sha256
