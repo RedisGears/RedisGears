f67ac3e1f21d2f27cc0a82efde2c0a4c3e7c8707
Meir Shpilraien (Spielrein) <meir@redis.com>
2023-05-15 21:44:00 +0300

    Add allow list to filter out globals. (#903)
    Fix #902 add allow list and deny list to filter JS globals.
    
    Any global which are not on the allow list will be removed. If the global is also not appear of the deny list, we will output a log message indicating we have a global which we do not considered and we should check the porpose of this global and decide if we want to filter it or not.
    
    List of allow globals:
    
    * Object
    * Function
    * Array
    * Number
    * parseFloat
    * parseInt
    * Infinity
    * NaN
    * undefined
    * Boolean
    * String
    * Symbol
    * Date
    * Promise
    * RegExp
    * Error
    * AggregateError
    * RangeError
    * ReferenceError
    * SyntaxError
    * TypeError
    * URIError
    * globalThis
    * JSON
    * Math
    * Intl
    * ArrayBuffer
    * Uint8Array
    * Int8Array
    * Uint16Array
    * Int16Array
    * Uint32Array
    * Int32Array
    * Float32Array
    * Float64Array
    * Uint8ClampedArray
    * BigUint64Array
    * BigInt64Array
    * DataView
    * Map
    * BigInt
    * Set
    * WeakMap
    * WeakSet
    * Proxy
    * Reflect
    * FinalizationRegistry
    * WeakRef
    * decodeURI
    * decodeURIComponent
    * encodeURI
    * encodeURIComponent
    * escape
    * unescape
    * isFinite
    * isNaN
    * console
    * WebAssembly
    
    List of deny globals:
    
    * eval - Might be considered dangerous.
    * EvalError - Sense we remove eval, this one is also not needed.
    * SharedArrayBuffer - Needed for web workers which we are not supporting
    * Atomics - Needed for web workers which we are not supporting
    
    Require - https://github.com/RedisGears/v8-rs/pull/22

---
+++ redisgears_macros_internals/src/allow_deny_lists.rs
@@ -0,0 +1,48 @@
+use std::collections::HashSet;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use serde::Deserialize;
+use serde_syn::{config, from_stream};
+use syn::{
+    parse::{Parse, ParseStream},
+    parse_macro_input,
+};
+
+#[derive(Debug, Deserialize)]
+struct Args {
+    allow_list: HashSet<String>,
+    deny_list: HashSet<String>,
+}
+
+impl Parse for Args {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        from_stream(config::JSONY, &input)
+    }
+}
+
+pub(crate) fn get_allow_deny_lists(item: TokenStream) -> TokenStream {
+    let args = parse_macro_input!(item as Args);
+
+    // verify no overlaps
+    let res = args
+        .allow_list
+        .iter()
+        .find(|v| args.deny_list.contains(v.as_str()));
+
+    if let Some(res) = res {
+        return quote! {compile_error!(std::stringify!(#res value appears on both, the allow list and the deny list.))}
+            .into();
+    };
+
+    let allow_list: Vec<_> = args.allow_list.into_iter().collect();
+    let deny_list: Vec<_> = args.deny_list.into_iter().collect();
+
+    quote! {
+        (
+            [#(#allow_list, )*].into_iter().map(|v| v.to_owned()).collect::<std::collections::HashSet<String>>(),
+            [#(#deny_list, )*].into_iter().map(|v| v.to_owned()).collect::<std::collections::HashSet<String>>()
+        )
+    }
+    .into()
+}
