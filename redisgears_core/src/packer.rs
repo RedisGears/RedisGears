320974081ea9d5e8934831579981b71031254211
meir <meir@redis.com>
2023-05-29 11:48:32 +0300

    Get the git tag for artifact name if exists.

--- redisgears_core/src/packer.rs
+++ redisgears_core/src/packer.rs
@@ -6,9 +6,7 @@
 
 use std::process::Command;
 
-pub const GIT_SHA: Option<&str> = std::option_env!("GIT_SHA");
-pub const GIT_BRANCH: Option<&str> = std::option_env!("GIT_BRANCH");
-pub const VERSION_STR: Option<&str> = std::option_env!("VERSION_STR");
+pub const GIT_BRANCH_OR_TAG: Option<&str> = std::option_env!("GIT_BRANCH_OR_TAG");
 pub const VERSION_NUM: Option<&str> = std::option_env!("VERSION_NUM");
 pub const BUILD_OS: Option<&str> = std::option_env!("BUILD_OS");
 pub const BUILD_OS_NICK: Option<&str> = std::option_env!("BUILD_OS_NICK");
@@ -36,21 +34,11 @@ fn main() {
         BUILD_OS.unwrap(),
         BUILD_OS_NICK.unwrap(),
         BUILD_OS_ARCH.unwrap(),
-        GIT_BRANCH.unwrap()
-    );
-    let gears_release_file_name = format!(
-        "redisgears2.{}-{}-{}.{}.zip",
-        BUILD_OS.unwrap(),
-        BUILD_OS_NICK.unwrap(),
-        BUILD_OS_ARCH.unwrap(),
-        VERSION_NUM.unwrap()
+        GIT_BRANCH_OR_TAG.unwrap()
     );
 
     let mut gears_snapeshot_file_path = curr_path.clone();
     gears_snapeshot_file_path.push(gears_snapeshot_file_name);
-
-    let mut gears_release_file_path = curr_path.clone();
-    gears_release_file_path.push(gears_release_file_name);
 
     if !Command::new("ramp")
         .args([
@@ -71,28 +59,6 @@ fn main() {
         .success()
     {
         println!("Failed ramp snapeshot");
-        std::process::exit(1);
-    }
-
-    if !Command::new("ramp")
-        .args([
-            "pack",
-            "--debug",
-            "-m",
-            ramp_yml_path.to_str().unwrap(),
-            "-o",
-            gears_release_file_path.to_str().unwrap(),
-            redisgears_so_path.to_str().unwrap(),
-        ])
-        .env(
-            "REDISGEARS_V8_PLUGIN_PATH",
-            redisgears_v8_plugin_so_path.to_str().unwrap(),
-        )
-        .status()
-        .expect("Failed ramp release")
-        .success()
-    {
-        println!("Failed ramp release");
         std::process::exit(1);
     }
 }
