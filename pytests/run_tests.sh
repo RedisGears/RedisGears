456d413445a21fa7d5275e96504450d364e1bc86
Victor Polevoy <fx@thefx.co>
2023-12-06 13:03:32 +0100

    Remove the manual RLtest installation

--- pytests/run_tests.sh
+++ pytests/run_tests.sh
@@ -6,13 +6,13 @@
 # - Python 3 pip.
 # - coreutils
 
-FULL_PATH_TO_SCRIPT="$(realpath "${BASH_SOURCE[-1]}")"
+FULL_PATH_TO_SCRIPT="$(realpath "${BASH_SOURCE:-$0}")"
 SCRIPT_DIRECTORY="$(dirname "$FULL_PATH_TO_SCRIPT")"
 CALLING_DIRECTORY=`pwd`
 
 printf "\e[31m Python will now install the dependencies system-wide.\e[0m\n"
 
-pip install -r $SCRIPT_DIRECTORY/requirements.txt --break-system-packages
+python3 -m pip install -r $SCRIPT_DIRECTORY/requirements.txt -U --break-system-packages
 
 cd $SCRIPT_DIRECTORY
 
