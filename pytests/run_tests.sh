785699305e1e761c2786c0278326e775888e5355
Victor Polevoy <fx@thefx.co>
2023-12-06 12:47:14 +0100

    Automatically install the python dependencies for the tests.
    The dependencies had to had been installed earlier manually, in order
    to run the python tests. This commit changes this so that the
    dependencies are installed automatically when invoking the
    "run_tests.sh" script.

--- pytests/run_tests.sh
+++ pytests/run_tests.sh
@@ -1,1 +1,17 @@
+#!/usr/bin/env sh
+#
+# Requires Python 3.
+
+FULL_PATH_TO_SCRIPT="$(realpath "${BASH_SOURCE[-1]}")"
+SCRIPT_DIRECTORY="$(dirname "$FULL_PATH_TO_SCRIPT")"
+CALLING_DIRECTORY=`pwd`
+
+printf "\e[31m Python will now install the dependencies system-wide.\e[0m\n"
+
+pip install -r $SCRIPT_DIRECTORY/requirements.txt --break-system-packages
+
+cd $SCRIPT_DIRECTORY
+
 RLTest $@
+
+cd $CALLING_DIRECTORY
