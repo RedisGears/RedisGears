22a3db9f122edafc1436eeb278a858ca30759b0d
Victor Polevoy <fx@thefx.co>
2023-12-06 13:21:44 +0100

    Use the 'break-system-packages' portably for pip 3.
    Makes the pip operate in a more portable and backwards compatible way
    when deciding whether to intervene with the system packages.

--- pytests/run_tests.sh
+++ pytests/run_tests.sh
@@ -6,13 +6,15 @@
 # - Python 3 pip.
 # - coreutils
 
+set -e
+
 FULL_PATH_TO_SCRIPT="$(realpath "${BASH_SOURCE:-$0}")"
 SCRIPT_DIRECTORY="$(dirname "$FULL_PATH_TO_SCRIPT")"
 CALLING_DIRECTORY=`pwd`
 
 printf "\e[31m Python will now install the dependencies system-wide.\e[0m\n"
 
-python3 -m pip install -r $SCRIPT_DIRECTORY/requirements.txt -U --break-system-packages
+PIP_BREAK_SYSTEM_PACKAGES=1 python3 -m pip install -r $SCRIPT_DIRECTORY/requirements.txt -U
 
 cd $SCRIPT_DIRECTORY
 
