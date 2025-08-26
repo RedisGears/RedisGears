63c53b5276348fff9da0317e4249d930f7368d8d
Gal Cohen <gal@redis.com>
2025-08-26 13:16:54 +0000

    Some cleanups

--- sbin/publish.py
+++ sbin/publish.py
@@ -42,7 +42,4 @@ def main():
 
 
 if __name__ == "__main__":
-    assert os.getenv("AWS_REGION") or os.getenv("AWS_DEFAULT_REGION")
-    assert os.getenv("AWS_ACCESS_KEY_ID")
-    assert os.getenv("AWS_SECRET_ACCESS_KEY")
     main()
